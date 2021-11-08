a82ea2a2f6028327909669b80fc33f78c4594e0a
Viachaslau Paulau <pavlov_box@tut.by>
2021-11-08 23:27:49 +0300

    added GET /users/:id/edit

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,16 @@
 import i18next from 'i18next';
 
 export default (app) => {
-  app
+  app // GET /users страница со списком всех пользователей
     .get('/users', { name: 'users' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
       reply.render('users/index', { users });
       return reply;
-    })
+    }) // GET /users/new - страница регистрации
     .get('/users/new', { name: 'newUser' }, (req, reply) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
-    })
+    }) // POST /users - создание пользователя
     .post('/users', async (req, reply) => {
       try {
         const user = await app.objection.models.user.fromJson(req.body.data);
@@ -25,9 +25,13 @@ export default (app) => {
         reply.render('users/new', { user: req.body.data, errors: data });
         return reply;
       }
-    })
-    .get('/users/1/edit', { name: 'editUser' }, (req, reply) => {
-      console.log('user id edit', req);
-      reply.render('users/edit', {});
+    }) // GET /users/:id/edit - страница редактирования пользователя
+    .get('/users/:idUser/edit', async (req, reply) => {
+      const { idUser } = req.params;
+      const [user] = await app.objection.models.user.query().where('id', idUser);
+      user.password = user.passwordDigest;
+      console.log('%%%%%%%%%%%%', user, user.passwordDigest);
+      reply.render('users/edit', { user });
+      return reply;
     });
 };
