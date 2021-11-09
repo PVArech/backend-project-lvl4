3e9f651d53692decf5d72cac4cef12802ce499fb
Viachaslau Paulau <pavlov_box@tut.by>
2021-11-09 22:52:17 +0300

    modified server/routes/users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,27 @@
 import i18next from 'i18next';
 
 export default (app) => {
-  app // GET /users страница со списком всех пользователей
+  app
     .get('/users', { name: 'users' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
       reply.render('users/index', { users });
       return reply;
-    }) // GET /users/new - страница регистрации
+    })
     .get('/users/new', { name: 'newUser' }, (req, reply) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
-    }) // POST /users - создание пользователя
+    })
+    .get('/users/:idUser/edit', { preValidation: app.authenticate }, async (req, reply) => {
+      const { idUser } = req.params;
+      const { id } = req.user;
+      if (Number(idUser) !== id) {
+        req.flash('error', i18next.t('flash.users.delete.error'));
+        return reply.redirect(app.reverse('users'));
+      }
+      const [user] = await app.objection.models.user.query().where('id', idUser);
+      reply.render('users/edit', { user });
+      return reply;
+    })
     .post('/users', async (req, reply) => {
       try {
         const user = await app.objection.models.user.fromJson(req.body.data);
@@ -26,7 +37,10 @@ export default (app) => {
         return reply;
       }
     })
-    // DELETE /users/:id - удаление пользователя
+    .patch('/users/:idUser', async (req, reply) => {
+      const { idUser } = req.params;
+      reply.send(`edit user ${idUser}`);
+    })
     .delete('/users/:idUser', { preValidation: app.authenticate }, async (req, reply) => {
       const { idUser } = req.params;
       const { id } = req.user;
@@ -37,15 +51,6 @@ export default (app) => {
       await app.objection.models.user.query().where('id', idUser).del();
       req.logOut();
       req.flash('info', i18next.t('flash.users.delete.success'));
-      reply.redirect(app.reverse('users'));
-    })
-    // GET /users/:id/edit - страница редактирования пользователя
-    .get('/users/:idUser/edit', async (req, reply) => {
-      const { idUser } = req.params;
-      const [user] = await app.objection.models.user.query().where('id', idUser);
-      // user.password = user.passwordDigest;
-      console.log('%%%%%%%%%%%%', user);
-      reply.render('users/edit', { user });
-      return reply;
+      return reply.redirect(app.reverse('users'));
     });
 };
